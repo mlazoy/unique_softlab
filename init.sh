0944310237df89c4371748d55f90340754c792db
mlazoy <mlazoy@icloud.com>
2025-03-31 18:28:35 +0300

    timestamps fixed

--- init.sh
+++ init.sh
@@ -12,9 +12,15 @@ for room_id in "${NUM_ROOMS[@]}"; do
     VERSION="ROOM${room_id}"  
     dirname="room${room_id}"
     mkdir -p "$ESCAPE_PATH/$dirname"
-    cp -r -p "./src/room${room_id}/." "$ESCAPE_PATH/$dirname"
+    cp -r "./src/room${room_id}/." "$ESCAPE_PATH/$dirname"
     echo "Compiling for $VERSION..."
     g++ -std=c++17 -D$VERSION ./src/escape.cpp -o "$ESCAPE_PATH/$dirname/play"
+    if [[ "$dirname" == "room6" ]]; then
+        touch -t 200703311430 Alex
+        touch -t 200903311430 Paul
+        touch -t 200403311430 Sophia
+        touch -t 200503311430 Jenny
+    fi
 done
 
 chmod 000 $ESCAPE_PATH"/room7/readme.txt"
